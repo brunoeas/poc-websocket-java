278542ec07619f1beaa69d74bda5397441c132d4
Bruno Eduardo <bruno.soares@kepha.com.br>
2020-01-06 09:09:24 -0300

    dependencias base

--- src/test/java/br/com/poc_websocket_java/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.poc_websocket_java;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
